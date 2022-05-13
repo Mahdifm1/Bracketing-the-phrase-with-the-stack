c63a1c669de201e01349f35205a925b60551c8a1
Mahdifm1 <80mahde@gmail.com>
2022-05-13 23:14:47 +0430

    add bracketsForNegativeSign function The negative number is placed between the brackets

--- main.cpp
+++ main.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
+#include "Stack.hpp"
+#include "Symbols.hpp"
+
+void bracketsForNegativeSign(Stack &inputStack) {
+    std::string inputString = inputStack.stackToString();
+    std::string stringReplace;
+    Symbols symbols;
+    int firstLen = inputString.length();
+    int count = 0;
+    for (int i = 0; i < inputString.length(); ++i) {
+        if (symbols.findSymbolPriority(inputString[i]) != 0) {
+            if (inputString[i + 1] == '-') {
+                stringReplace = "(-";
+                int j, NumberDigits=0;
+                for (j = i + 2; (inputString[j] >= 48 && inputString[j] <= 57) || inputString[j] == '.'; ++j) {
+                    stringReplace += inputString[j];
+                    NumberDigits++;
+                }
+                stringReplace += ")";
+
+                inputString.replace(j - 2 , NumberDigits + 1, stringReplace);
+                std::cout <<inputString<< "\n";
+            }
+        }
+    }
+}
 
 int main() {
     std::string input;
