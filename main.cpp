ac517d9b41c802561eb6b3b26089e8b20a6308b9
Mahdifm1 <80mahde@gmail.com>
2022-05-14 01:18:35 +0430

    fix bracketsForNegativeSign function bugs for functions

--- main.cpp
+++ main.cpp
@@ -8,8 +8,49 @@ void bracketsForNegativeSign(Stack &inputStack) {
     Symbols symbols;
 
     for (int i = 0; i < inputString.length(); ++i) {
-        if (symbols.findSymbolPriority(inputString[i]) != 0) {
-            if (inputString[i + 1] == '-') {
+        if (inputString[i] == '(' && inputString[i + 1] == '-') { //for (-sin15 + ...
+            int countSymbol = 0;
+            stringReplace = "(";
+            int j;
+            for (j = i + 1; countSymbol < 2; ++j) {
+                if (symbols.findSymbolPriority(inputString[j]) != 0 || inputString[i] == ')') {
+                    countSymbol++;
+                    if (countSymbol == 2)
+                        break;
+                }
+
+                stringReplace += inputString[j];
+            }
+            stringReplace += ")";
+
+
+            if (symbols.findSymbolPriority(inputString[j + 1]) == 0) {
+                inputString.replace(i + 1, j - i - 1, stringReplace);
+                i++;
+            }
+
+            std::cout << inputString << "\n";
+        } else if (symbols.findSymbolPriority(inputString[i]) != 0) { //for *-15 +...
+            if (((inputString[i + 2] >= 65 && inputString[i + 2] <= 90) ||
+                 (inputString[i + 2] >= 97 && inputString[i + 2] <= 122)) && inputString[i + 1] == '-') { // for *-sin15 ...
+                stringReplace = "(-";
+                int j, numberWords = 0, numberDigits = 0;
+                for (j = i + 2; j < (inputString[j] >= 65 && inputString[j] <= 90) ||
+                                (inputString[j] >= 97 && inputString[j] <= 122); ++j) {
+                    stringReplace += inputString[j];
+                    numberWords++;
+                }
+                stringReplace += "(";
+                for (; (inputString[j] >= 48 && inputString[j] <= 57) || inputString[j] == '.'; ++j) {
+                    stringReplace += inputString[j];
+                    numberDigits++;
+                }
+                stringReplace += "))";
+
+//                std::cout << stringReplace << "\n";
+
+                inputString.replace(i + 1, numberWords + numberDigits + 1, stringReplace);
+            } else if (inputString[i + 1] == '-') { //for *-15 + ...
                 stringReplace = "(-";
                 int j, numberDigits = 0;
                 for (j = i + 2; (inputString[j] >= 48 && inputString[j] <= 57) || inputString[j] == '.'; ++j) {
@@ -18,10 +59,11 @@ void bracketsForNegativeSign(Stack &inputStack) {
                 }
                 stringReplace += ")";
 
-                inputString.replace(j - numberDigits - 1, numberDigits + 1, stringReplace);
-//                std::cout << inputString << " J " << j - numberDigits-1 << " N " << numberDigits << "\n";
+                inputString.replace(i + 1, numberDigits + 1, stringReplace);
+//                std::cout << inputString << " J " << j - numberDigits - 1 << " N " << numberDigits << "\n";
             }
         }
+
     }
     for (int i = 0; i < inputString.length(); ++i) {
         inputStack.push(inputString[i]);
@@ -37,6 +79,6 @@ int main() {
 
     bracketsForNegativeSign(inputStack);
 
-    std::cout<<inputStack.stackToString();
+    std::cout << inputStack.stackToString();
     return 0;
 }
